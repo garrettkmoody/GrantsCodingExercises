24d5a1c64c4a06dda480aef6347883562392095f
Garrett Moody <garrettmoody@Garretts-Air.home>
2021-06-24 00:09:06 -0700

    added problem 2

---
+++ Problem 2/problem2.py
@@ -0,0 +1,21 @@
+# Count how many times a certain letter appears
+# in the list
+thelist = ['f','s','j','u','k','y','t','h','s','r','e','w','h','y','i','o','l','p','g','k','m','n','b','g','g','f','d','e','g','t','y','r','e','w','q','a','s','w','q','d','e','d','c','z','z','x','c','g']
+
+
+
+def countOccurences(letter, list):
+    times = 0
+    #Finish the code here
+
+    return times
+
+
+
+
+
+
+
+theLetter = input("Enter the letter: ")
+
+print(countOccurences(theLetter, thelist))
